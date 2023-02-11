f65603fb59b75e8d63a09f926b00c42b86e0a954
ferchovas20 <ferchovas20@gmail.com>
2023-02-11 04:22:16 -0600

    Aplicacion suma de dos numeros con js y bootstrap5

---
+++ Aplicacion_suma_de_dos_numeros/app.js
@@ -0,0 +1,14 @@
+
+function sumar(){
+    const formulario = document.getElementById("formulario");
+    let numero1 = formulario["numero1"];
+    let numero2 = formulario["numero2"];
+    let resultado = parseInt(numero1.value) + parseInt(numero2.value);
+
+    if( isNaN(resultado) ){
+        resultado = "Debes agregar los números";
+    }
+
+    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
